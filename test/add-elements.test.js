15507d764fbc590afbd016d8d82671851b2397ef
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2021-05-28 14:39:09 +0200

    added tests for add-elements.js

--- test/add-elements.test.js
+++ test/add-elements.test.js
@@ -15,12 +15,6 @@ describe('AddElements function', () => {
             addElements.setReg('CY000000')
             assert.equal('CY000000', addElements.getReg())
         });
-        
-        it('should create an element to store the registration number', () => {
-            let addElements = AddElements()
-            addElements.createRegElem('h2')
-            assert.deepEqual('<h2></h2>', addElements.getRegElem())
-        });
 
         it('ashould set the parent element to append the child element to', () => {
             let addElements = AddElements()
@@ -28,7 +22,35 @@ describe('AddElements function', () => {
             assert.equal('div', addElements.getParent())
         });
         
-        
     });
-    
-});
+
+    describe('Warning Messages', () => {
+        it('should display error if there is no registration number provided', () => {
+            let addElements = AddElements()
+
+            addElements.setError('Registration number cannot be empty')
+            assert.equal('Registration number cannot be empty', addElements.getError())
+        });
+
+        it('should display an error if the registration number provided is not valid', () => {
+            let addElements = AddElements()
+
+            addElements.setError('Please enter valid registration number')
+            assert.equal('Please enter valid registration number', addElements.getError())
+        });
+        
+        it('should return an error if the registration number entered is not from Cape Town, Bellville or Paarl', () => {
+            let addElements = AddElements()
+
+            addElements.setError('Please enter Cape Town, Bellvile or Paarl registration number')
+            assert.equal('Please enter Cape Town, Bellvile or Paarl registration number', addElements.getError())
+        });
+
+        it('should return error if the registration number added has been entered before', () => {
+            let addElements = AddElements()
+
+            addElements.setError('Registration Number already added. Please add unique registration number')
+            assert.equal('Registration Number already added. Please add unique registration number', addElements.getError())
+        })
+    })
+})
